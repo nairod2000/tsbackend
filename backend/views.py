1249f2cd6a9cf3073e7c445f568e12f22faf8ef7
dorian <dojmorrissey@gmail.com>
2024-05-23 14:20:23 -0500

    removed unused token views and serializer

--- backend/views.py
+++ backend/views.py
@@ -13,60 +13,11 @@ from django.contrib.auth.models import User
 from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .serializers import CustomTokenObtainPairSerializer
 from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 
 import logging
 
 logger = logging.getLogger(__name__)
-
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        data = serializer.validated_data
-
-        response = Response(data, status=status.HTTP_200_OK)
-        response.set_cookie(
-            key='access_token',
-            value=data['access'],
-            httponly=True,
-            secure=False,  # Set to True in production
-            samesite='Lax',
-        )
-        response.set_cookie(
-            key='refresh_token',
-            value=data['refresh'],
-            httponly=True,
-            secure=False,  # Set to True in production
-            samesite='Lax',
-        )
-        return response
-
-class CustomTokenRefreshView(TokenRefreshView):
-    serializer_class = TokenRefreshSerializer
-
-    def post(self, request, *args, **kwargs):
-        refresh_token = request.COOKIES.get('refresh_token')
-        if not refresh_token:
-            return Response({'error': 'Refresh token not found in cookies'}, status=status.HTTP_400_BAD_REQUEST)
-
-        serializer = self.get_serializer(data={'refresh': refresh_token})
-        serializer.is_valid(raise_exception=True)
-        data = serializer.validated_data
-
-        response = Response(data, status=status.HTTP_200_OK)
-        response.set_cookie(
-            key='access_token',
-            value=data['access'],
-            httponly=True,
-            secure=False,  # Set to True in production
-            samesite='Lax',
-        )
-        return response
 
 
 @api_view(['POST'])
